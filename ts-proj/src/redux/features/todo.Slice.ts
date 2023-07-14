fe7d339a09f24d1a628b735307c76d268b58f9ae
Standa Vin <vinninjaq@gmail.com>
2023-07-15 00:11:46 +0200

    Reducers

--- ts-proj/src/redux/features/todo.Slice.ts
+++ ts-proj/src/redux/features/todo.Slice.ts
@@ -1,12 +1,12 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
-interface ITodo {
+interface ITodo { // тип элемента массива
     id: string;
     text: string;
     complited: boolean;
 }
 
-interface TodoState {
+interface TodoState { // тип массива
     todos: ITodo[];
 }
 
@@ -18,6 +18,24 @@ export const todoSlice = createSlice({
     name: 'todo',
     initialState,
     reducers: {
-        
+        addTodo: (state, action: PayloadAction<string>) => { // добавляем элемент в массив todos
+            state.todos.push({
+                id: new Date().toISOString(),
+                text: action.payload,
+                complited: false
+            })
+        },
+        removeTode: (state, action: PayloadAction<string>) => { // удаляем элемент из массива todoos 
+            state.todos = state.todos.filter(todo => todo.id !== action.payload);
+        },
+        markAsDone: (state, action: PayloadAction<string>) => { // ставим чекбокс в состояние выполнено или не выполнено
+            const toggledTodo = state.todos.find(todo => todo.id === action.payload);
+            if (toggledTodo) {
+                toggledTodo.complited = !toggledTodo.complited;
+            }
+        }
     }
-})
+});
+
+export const { addTodo, removeTode, markAsDone } = todoSlice.actions; // экспорт редусерав
+export default todoSlice.reducer; // экспорт редюсера
