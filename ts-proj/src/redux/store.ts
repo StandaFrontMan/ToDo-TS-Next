af3162bebd3929276dc8c4129aad844db207e228
Standa Vin <vinninjaq@gmail.com>
2023-07-14 23:09:48 +0200

    RootState and AppDispatch in store

--- ts-proj/src/redux/store.ts
+++ ts-proj/src/redux/store.ts
@@ -1,11 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import todoReducer from "./features/todoSlice";
 
 export const store = configureStore({
     reducer: {
-        todoReducer
+
     }
 });
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState> 
+export type AppDispatch = typeof store.dispatch //  экспортируем типы RootState и AppDispatch, чтобы они были доступны в других частях приложения
