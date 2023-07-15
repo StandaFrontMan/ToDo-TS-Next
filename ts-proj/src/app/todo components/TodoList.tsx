ee98a79f437f4a9e306c977a11df2255648b231a
Standa Vin <vinninjaq@gmail.com>
2023-07-15 03:14:01 +0200

    Implementation of filtering by the criterion of completed

--- ts-proj/src/app/todo components/TodoList.tsx
+++ ts-proj/src/app/todo components/TodoList.tsx
@@ -1,25 +1,34 @@
 import { useAppSelector } from '@/redux/hooks'
-import React from 'react'
+import React, { useState } from 'react'
 import TodoItem from './TodoItem'
 
 const TodoList: React.FC = () => {
 
     const { todos } = useAppSelector(state => state.todo)
 
+    const [filter, setFilter] = useState<'completed' | 'uncompleted' | 'all'>('all');
 
-    console.log(todos);
+    const filteredTodos = todos.filter((todo) => { // реализация фильтрации по кретерию condition
+        if (filter === 'completed') {
+          return todo.complited;
+        } else if (filter === 'uncompleted') {
+          return !todo.complited;
+        } else {
+          return true; 
+        }
+      });
     
   return (
-    <ul>
-        {
-            todos.map(todo => (
-                <TodoItem
-                    key={todo.id}
-                    {...todo}
-                />
-            ))
-        }
-    </ul>
+    <div>
+      <button onClick={() => setFilter('completed')}>Show Complited Todos</button>
+      <button onClick={() => setFilter('uncompleted')}>Show Uncomplited Todos</button>
+      <button onClick={() => setFilter('all')}>Show All Todos</button>
+      <ul>
+        {filteredTodos.map((todo) => (
+          <TodoItem key={todo.id} {...todo} />
+        ))}
+      </ul>
+    </div>
   )
 }
 
